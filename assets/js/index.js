5d1a383c0f788f785b98a1b67b7f8af63bc879dc
Liuyq <3175217690@qq.com>
2022-04-18 10:37:37 +0800

    some update

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,17 +36,17 @@ function getUserInfo() {
     },
     // 不论成功还是失败，最终都会调用 complete 回调函数
     // 控制用户的访问权限
-    // complete: function (res) {
-    //   // console.log('执行了 complete 回调函数');
-    //   // console.log(res);
-    //   // 在 complete 回调函数中可以使用 res.responseJSON 拿到服务器响应回来的数据
-    //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-    //     // 1.强制清空token
-    //     localStorage.removeItem('token')
-    //     // 2.强制跳转到登录页面
-    //     location.href = '/login.html'
-    //   }
-    // }
+    complete: function (res) {
+      // console.log('执行了 complete 回调函数');
+      // console.log(res);
+      // 在 complete 回调函数中可以使用 res.responseJSON 拿到服务器响应回来的数据
+      if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        // 1.强制清空token
+        localStorage.removeItem('token')
+        // 2.强制跳转到登录页面
+        location.href = '/login.html'
+      }
+    }
   })
 }
 
